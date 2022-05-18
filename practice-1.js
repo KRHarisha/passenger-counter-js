3dd77116db00f920c14d5074135afa58592e53e0
KRHarisha <105624826+KRHarisha@users.noreply.github.com>
2022-05-18 12:42:55 +0530

    Add files via upload

---
+++ practice-1.js
@@ -0,0 +1,103 @@
+// /*1. Variables practice */
+// // Create two variables, firstName and lastName
+// let firstName = "Harisha"
+// let lastname = "Kelam"
+
+// // Concatenate the two variables into a third variable called fullName
+// function fullName()
+// {
+//     console.log(firstName + " " + lastname)
+// }
+
+// // Log fullName to the console
+// fullName()
+
+/*2. Concatenate two strings in a function */
+// let name = "Linda"
+// let greeting = "Hi there"
+
+// // Create a function that logs out "Hi there, Linda!" when called
+// function greetingsLog()
+// {
+//     console.log(greeting + "," + " " + name + "!")
+// }
+// greetingsLog()
+
+/*3. Incrementing and decrementing */
+let myPoints = 3
+
+// Create two functions, add3Points() and remove1Point(), and have them
+// add/remove points to/from the myPoints variable
+// function add3Points()
+// {
+//    myPoints += 3
+// }
+// function remove1Point()
+// {
+//     myPoints -= 1
+// }
+// add3Points()
+// add3Points()
+// add3Points()
+// remove1Point()
+// remove1Point()
+
+
+// // Call the functions to that the line below logs out 10
+// console.log(myPoints)
+
+/*4. Strings and numbers */
+// Try to predict what each of the lines will log out
+// console.log("2" + 2) //22
+// console.log(11 + 7) //18
+// console.log(6 + "5") //65
+// console.log("My points: " + 5 + 9) //My Points 59
+// console.log(2 + 2) // 4
+// console.log("11" + "14") // 1114
+/*5. Rendering an error message */
+// When the user clicks the purchase button, render out
+// "Something went wrong, please try again" in the paragraph
+// that has the id="error".
+// let errorParagraph = document.getElementById("error")
+// console.log(errorParagraph)
+// function purchase()
+// {
+//     console.log("button clicked")
+//     errorParagraph.textContent = "Something went wrong, please try again"
+// }
+
+/*  6. Calculator challenge */
+// let num1 = 8
+// let num2 = 2
+// document.getElementById("num1-en").textContent = num1
+// document.getElementById("num2-en").textContent = num2
+// // Create four functions: add(), subtract(), divide(), multiply()
+// // Call the correct function when the user clicks on one of the buttons
+// // Perform the given calculation using num1 and num2
+// // Render the result of the calculation in the paragraph with id="sum-el"
+
+// // E.g. if the user clicks on the "Plus" button, you should render
+// // "Sum: 10" (since 8 + 2 = 10) inside the paragraph with id="sum-el"
+// let sumEn = document.getElementById("sum-en")
+// function add()
+// {
+//     let result = num1 + num2
+//     sumEn.textContent = "sum: " + result
+//     // console.log("add clicked")
+// }
+// function subtract()
+// {
+//     let result = num1 - num2
+//     sumEn.textContent = "sum: " + result
+// }
+// function divide()
+// {
+//     let result = num1 / num2
+//     sumEn.textContent = "sum: " + result
+// }
+// function multiply()
+// {
+//     let result = num1 * num2
+//     sumEn.textContent = "sum: " + result
+// }
+/* 7. Figma tutorial */
